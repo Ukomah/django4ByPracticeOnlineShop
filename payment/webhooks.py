0daa032c8cdc2488eb37ced23ee9b52956224617
Ukomah <henryukomah@gmail.com>
2023-07-05 10:32:08 +0100

    creating invoice and feedback messages

--- payment/webhooks.py
+++ payment/webhooks.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from orders.models import Order
-
+from .tasks import payment_completed
 
 @csrf_exempt
 def stripe_webhook(request):
@@ -28,11 +28,16 @@ def stripe_webhook(request):
                 order = Order.objects.get(id=session.client_reference_id)
             except Order.DoesNotExist:
                 return HttpResponse(status=404)
+            
             # mark order as paid
             order.paid = True
+            
             # store Stripe payment ID
             order.stripe_id = session.payment_intent
             order.save()
+            
+            # launch asynchronous task
+            payment_completed.delay(order.id)
             
     return HttpResponse(status=200)
     
